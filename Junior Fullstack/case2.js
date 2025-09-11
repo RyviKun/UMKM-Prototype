605066fd65b8c6d8102a9f1755bd63ca89bce03c
Ryvi <136465518+RyviKun@users.noreply.github.com>
2025-09-11 11:24:40 +0700

    Update case2.js

--- Junior Fullstack/case2.js
+++ Junior Fullstack/case2.js
@@ -1,51 +1,51 @@
-comments = [
-{
-commentId: 1,
-commentContent: 'Hai',
-replies: [
-{
-commentId: 11,
-commentContent: 'Hai juga',
-replies: [
-{
-commentId: 111,
-commentContent: 'Haai juga hai jugaa'
-},
-{
-commentId: 112,
-commentContent: 'Haai juga hai jugaa'
-}
-]
-},
-{
-commentId: 12,
-commentContent: 'Hai juga',
-replies: [
-{
-commentId: 121,
-commentContent: 'Haai juga hai jugaa'
-}
-]
-}
-]
-},
-{
-commentId: 2,
-commentContent: 'Halooo'
-}
-]
+const comments = [
+  {
+    commentId: 1,
+    commentContent: 'Hai',
+    replies: [
+      {
+        commentId: 11,
+        commentContent: 'Hai juga',
+        replies: [
+          {
+            commentId: 111,
+            commentContent: 'Haai juga hai jugaa'
+          },
+          {
+            commentId: 112,
+            commentContent: 'Haai juga hai jugaa'
+          }
+        ]
+      },
+      {
+        commentId: 12,
+        commentContent: 'Hai juga',
+        replies: [
+          {
+            commentId: 121,
+            commentContent: 'Haai juga hai jugaa'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    commentId: 2,
+    commentContent: 'Halooo'
+  }
+];
 
 
+//menghitung total comment secara rekursif dengan memanggil fungsi sendiri, 
 function countComments(comments){
     let total = 0;
-    
     comments.forEach(
         item => {
-            
                 total += 1;
+                //jika didalam comment tersebut mempunyai replies, maka fungsu countComments akan dipanggil lagi
                 if (item.replies){
+                  //menambahkan total yang dihitung dari fungsi tersebut
                     total += countComments(item.replies);
-                
             }
         })
     return total;
